9499ea74bc8b6947571d8d2b504c1e5bc7340b16
murphbra <murphbra@oregonstate.edu>
2021-07-19 14:58:16 -0600

    update

--- app.js
+++ app.js
@@ -3,6 +3,12 @@
 /*
     SETUP
 */
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', exphbs({                     // Create an instance of the handlebars engine to process templates
+    extname: ".hbs"
+}));
+app.set('view engine', '.hbs'); 
+
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
 var db = require('./database/db-connector')
@@ -13,32 +19,10 @@ PORT        = 5461;                 // Set a port number at the top so it's easy
 */
 app.get('/', function(req, res)                 // This is the basic syntax for what is called a 'route'
     {
-        // Define our queries
-        query1 = 'DROP TABLE IF EXISTS diagnostic;';
-        query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
-        query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
-        query4 = 'SELECT * FROM diagnostic;';
-
-        // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
-
-        // DROP TABLE...
-        db.pool.query(query1, function (err, results, fields){
-
-            // CREATE TABLE...
-            db.pool.query(query2, function(err, results, fields){
-
-                // INSERT INTO...
-                db.pool.query(query3, function(err, results, fields){
-
-                    // SELECT *...
-                    db.pool.query(query4, function(err, results, fields){
-
-                        // Send the results to the browser
-                        res.send(JSON.stringify(results));
-                    });
-                });
-            });
-        });
+        app.get('/', function(req, res)
+    {
+        res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+    });                                         // will process this file, before sending the finished HTML to the client.
     });                                         // requesting the web site.
 
 /*
