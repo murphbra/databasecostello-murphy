62f24f7f04a9368774663fc5ff15bb5d70143adf
murphbra <murphbra@oregonstate.edu>
2021-07-19 15:13:43 -0600

    update

--- app.js
+++ app.js
@@ -3,27 +3,26 @@
 /*
     SETUP
 */
-var express = require('express');   // We are using the express library for the web server
-var app     = express();            // We need to instantiate an express object to interact with the server in our code
+var express = require('express');   
+var app     = express();            
 var db = require('./database/db-connector')
-PORT        = 5461;                 // Set a port number at the top so it's easy to change in the future
-var exphbs = require('express-handlebars');     // Import express-handlebars
-app.engine('.hbs', exphbs({                     // Create an instance of the handlebars engine to process templates
+PORT        = 5461;                 
+var exphbs = require('express-handlebars');     
+app.engine('.hbs', exphbs({                     
     extname: ".hbs"
 }));
 app.set('view engine', '.hbs'); 
+app.set('port', 5461);
+
 /*
     ROUTES
 */
-app.get('/', function(req, res)                 // This is the basic syntax for what is called a 'route'
-    {
-        res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
-    });                                         // will process this file, before sending the finished HTML to the client.
-                                         
-
+app.get('/', function(req, res){
+        res.render('index');                    
+    });                                        
 /*
     LISTENER
 */
-app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+app.listen(PORT, function(){            
     console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
 });
