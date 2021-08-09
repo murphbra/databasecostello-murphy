6038b528b356f816dab384f4f200b4b176778a54
murphbra <murphbra@oregonstate.edu>
2021-08-09 16:35:34 -0600

    update

--- app.js
+++ app.js
@@ -111,6 +111,20 @@ app.get('/PropertyOwners', function(req, res){
     })                    
     });  
 
+app.post('/PropertyOwners', function(req, res){
+    var mysql = req.app.get('mysql');
+    var sql = "INSERT INTO PropertyOwners (fname, lname, email) VALUES (?,?,?)";
+    var inserts = [req.body.fname, req.body.lname, req.body.email];
+    sql = db.pool.query (sql, inserts, function(error, results, fields){
+        if(error){
+            res.write(JSON.stringify(error));
+            res.end();
+        }else{
+            res.redirect('PropertyOwners');
+        }
+    })
+})
+
 app.get('/CrewLeaders', function(req, res){
     let query1 = "SELECT * FROM CrewLeaders;"; 
 
