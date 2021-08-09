919cca0cb5b6254f3123931535cfcd00bd7e9358
costjose <costjose@oregonstate.edu>
2021-08-09 16:53:19 -0700

    Update app.js

--- app.js
+++ app.js
@@ -145,7 +145,7 @@ app.get('/propertiesUpdate/:propertyID', function(req, res){
 
     app.post('/propertiesUpdate/:propertyID', function(req, res){
         var mysql = req.app.get('mysql');
-        var sql = "UPDATE Properties SET propAddress = ? WHERE id = ?;";
+        var sql = "UPDATE Properties SET propAddress = ? WHERE propertyID = ?;";
         var inserts = [req.body.addressInput, req.params.propertyID]
         sql = db.pool.query (sql, inserts, function(error, results, fields){
             if(error){
