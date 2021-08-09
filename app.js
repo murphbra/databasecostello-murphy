6e6e3ee058e526d2ed8f3f9f4123b345fb9fc4d9
murphbra <murphbra@oregonstate.edu>
2021-08-09 14:49:03 -0600

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var app     = express();
 var db = require('./database/db-connector');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
-PORT        = 5468;
+PORT        = 5467;
 
 app.engine('.hbs', exphbs({                     
     extname: ".hbs"
