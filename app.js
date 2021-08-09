18b7065d484b2f21e61b3b3e40c8c84ada3dc000
murphbra <murphbra@oregonstate.edu>
2021-08-09 13:24:05 -0600

    update to add new properties

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ var express = require('express');
 var app     = express();          
 var db = require('./database/db-connector');
 var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
 PORT        = 5467;
 
 app.engine('.hbs', exphbs({                     
     extname: ".hbs"
 }));
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', '.hbs'); 
 
 /*
@@ -31,6 +33,20 @@ app.get('/properties', function(req, res){
             res.render('properties', {data: rows}); 
         })                        
     });  
+
+app.post('/properties', function(req, res){
+    var mysql = req.app.get('mysql');
+    var sql = "INSERT INTO Properties (propAddress) VALUES (?)";
+    var inserts = [req.body.propAddress]
+    sql = mysql.pool.query (sql, inserts, function(error, results, fields){
+        if(error){
+            res.write(JSON.stringify(error));
+            res.end();
+        }else{
+            res.redirect('properties');
+        }
+    })
+})
 
 app.get('/CompletedLandscapingSessions', function(req, res){
     let query1 = "SELECT * FROM CompletedLandscapingSessions;"; 
