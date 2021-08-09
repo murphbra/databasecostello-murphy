b7568d1b1b97c7e48e359c3abace0ec2046c6591
murphbra <murphbra@oregonstate.edu>
2021-08-09 15:09:14 -0600

    Update app.js

--- app.js
+++ app.js
@@ -76,9 +76,9 @@ app.post('/properties', function(req, res){
 app.get('/CompletedLandscapingSessions', function(req, res){
     var callbackCount = 0;
     var context = {};
-    var db = req.app.get('db');
-    getLandscapingSessions(res, db, context, complete);
-    getProperties(res, db, context, complete);
+    var mysql = req.app.get('mysql');
+    getLandscapingSessions(res, mysql, context, complete);
+    getProperties(res, mysql, context, complete);
     function complete(){
         callbackCount++;
         if(callbackCount >= 2){
