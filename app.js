fc57a98b9ee3aea5c7a2bc3bd58a5dea5f083afd
costjose <costjose@oregonstate.edu>
2021-08-09 18:40:11 -0700

    10

--- app.js
+++ app.js
@@ -9,11 +9,9 @@ var db = require('./database/db-connector');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 PORT        = 5467;
-
 app.engine('.hbs', exphbs({                     
     extname: ".hbs"
 }));
-
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', '.hbs'); 
 
@@ -31,6 +29,17 @@ function getProperties (res, context, complete){
     });
 }
 
+function getOwners (res, context, complete){
+    db.pool.query("SELECT ownerID, fname, lname, email FROM PropertyOwners;", function(error, results, fields){
+        if(error){
+            res.write(JSON.stringify(error));
+            res.end();
+        }
+        context.owners = results;
+        complete();
+    });
+}
+
 function getLandscapingSessions (res, context, complete){
     db.pool.query("SELECT sessionID, sessionDate, Properties.propAddress AS address FROM CompletedLandscapingSessions JOIN Properties ON CompletedLandscapingSessions.propertyID = Properties.propertyID;", function(error, results, fields){
         if(error){
@@ -40,6 +49,52 @@ function getLandscapingSessions (res, context, complete){
         context.sessions = results;
         complete();
     });
+}
+
+function getCrewLeaders (res, context, complete){
+    db.pool.query("SELECT fname, lname, phoneNumber, leaderID, Properties.propAddress AS address FROM CrewLeaders JOIN Properties ON CrewLeaders.propertyID = Properties.propertyID;", function(error, results, fields){
+        if(error){
+            res.write(JSON.stringify(error));
+            res.end();
+        }
+        context.CrewLeaders = results;
+        complete();
+    });
+}
+
+function getEmployees(res, context, complete){
+    db.pool.query("SELECT Employees.fname, Employees.lname, Employees.phoneNumber, CrewLeaders.fname AS leaderfirst, CrewLeaders.lname AS leaderlast FROM Employees JOIN CrewLeaders ON Employees.leaderID = CrewLeaders.leaderID;", function(error, results, fields){
+        if(error){
+            res.write(JSON.stringify(error));
+            res.end();
+        }
+        context.employees = results;
+        complete();
+    });
+}
+
+function getPropertyOwned (res, context, complete){
+    db.pool.query("SELECT PropertyOwned.propertyID, PropertyOwned.ownerID, Properties.propAddress AS address, PropertyOwners.fname AS ownerfirst, PropertyOwners.lname AS ownerlast FROM PropertyOwned JOIN Properties ON PropertyOwned.propertyID = Properties.propertyID JOIN PropertyOwners ON PropertyOwned.ownerID = PropertyOwners.ownerID;", function(error, results, fields){
+        if(error){
+            res.write(JSON.stringify(error));
+            res.end();
+        }
+        context.propertyowned = results;
+        complete();
+    });
+}
+
+function deleteProperties(propertyID, res){
+    var sql = "DELETE FROM properties WHERE id = ?";
+    var inserts = [req.params.propertyID];
+    db.pool.query(sql, inserts, function(error, results, fields){
+        if(error){
+            res.write(JSON.stringify(error));
+            res.status(400);
+            res.end();
+        }
+        complete();
+    })
 }
 
 /*
@@ -111,52 +166,134 @@ app.get('/PropertyOwners', function(req, res){
     })                    
     });  
 
+app.post('/PropertyOwners', function(req, res){
+    var mysql = req.app.get('mysql');
+    var sql = "INSERT INTO PropertyOwners (fname, lname, email) VALUES (?,?,?)";
+    var inserts = [req.body.fname, req.body.lname, req.body.email];
+    sql = db.pool.query (sql, inserts, function(error, results, fields){
+        if(error){
+            res.write(JSON.stringify(error));
+            res.end();
+        }else{
+            res.redirect('PropertyOwners');
+        }
+    })
+})
+
 app.get('/CrewLeaders', function(req, res){
-    let query1 = "SELECT * FROM CrewLeaders;"; 
+    var callbackCount = 0;
+    var context = {};
+    //var mysql = req.app.get('db');
+    getCrewLeaders(res, context, complete);
+    getProperties(res, context, complete);
+    //res.render('CompletedLandscapingSessions', context); 
+    function complete(){
+        callbackCount++;
+        if(callbackCount >= 2){
+            res.render('CrewLeaders', context); 
+        }
+    }      
+                   
+});  
 
-    db.pool.query(query1, function(error, rows, fields){
-
-        res.render('CrewLeaders', {data: rows}); 
-    })                          
-    });  
+app.post('/CrewLeaders', function(req, res){
+    var mysql = req.app.get('mysql');
+    var sql = "INSERT INTO CrewLeaders (fname, lname, phoneNumber, propertyID) VALUES (?,?,?,?)";
+    var inserts = [req.body.fname, req.body.lname, req.body.phoneNumber, req.body.propertyID];
+    sql = db.pool.query (sql, inserts, function(error, results, fields){
+        if(error){
+            res.write(JSON.stringify(error));
+            res.end();
+        }else{
+            res.redirect('CrewLeaders');
+        }
+    })
+})
 
 app.get('/Employees', function(req, res){
-    let query1 = "SELECT * FROM Employees;"; 
+    var callbackCount = 0;
+    var context = {};
+    //var mysql = req.app.get('db');
+    getCrewLeaders(res, context, complete);
+    getEmployees(res, context, complete);
+    //res.render('CompletedLandscapingSessions', context); 
+    function complete(){
+        callbackCount++;
+        if(callbackCount >= 2){
+            res.render('Employees', context); 
+        }
+    }                 
+});  
 
-    db.pool.query(query1, function(error, rows, fields){
-
-        res.render('Employees', {data: rows}); 
-    })                        
-    });  
+app.post('/Employees', function(req, res){
+    var mysql = req.app.get('mysql');
+    var sql = "INSERT INTO Employees (fname, lname, phoneNumber, leaderID) VALUES (?,?,?,?)";
+    var inserts = [req.body.fname, req.body.lname, req.body.phoneNumber, req.body.leaderID];
+    sql = db.pool.query (sql, inserts, function(error, results, fields){
+        if(error){
+            res.write(JSON.stringify(error));
+            res.end();
+        }else{
+            res.redirect('Employees');
+        }
+    })
+})
 
 app.get('/PropertyOwned', function(req, res){
-    let query1 = "SELECT * FROM PropertyOwned;"; 
+    var callbackCount = 0;
+    var context = {};
+    //var mysql = req.app.get('db');
+    getProperties(res, context, complete);
+    getOwners(res, context, complete);
+    getPropertyOwned(res, context, complete);
+    //res.render('CompletedLandscapingSessions', context); 
+    function complete(){
+        callbackCount++;
+        if(callbackCount >= 3){
+            res.render('PropertyOwned', context); 
+        }
+    }          
+    });  
 
-    db.pool.query(query1, function(error, rows, fields){
+app.post('/PropertyOwned', function(req, res){
+    var mysql = req.app.get('mysql');
+    var sql = "INSERT INTO PropertyOwned (propertyID, ownerID) VALUES (?,?)";
+    var inserts = [req.body.propertyID, req.body.ownerID];
+    sql = db.pool.query (sql, inserts, function(error, results, fields){
+        if(error){
+            res.write(JSON.stringify(error));
+            res.end();
+        }else{
+            res.redirect('PropertyOwned');
+        }
+    })
+})
 
-        res.render('PropertyOwned', {data: rows}); 
-    })   
-});
     
 app.get('/propertiesUpdate/:propertyID', function(req, res){
     
             res.render('propertiesUpdate');
     });  
 
-    app.post('/propertiesUpdate/:propertyID', function(req, res){
-        var mysql = req.app.get('mysql');
-        var sql = "UPDATE Properties SET propAddress = ? WHERE propertyID = ?;";
-        var inserts = [req.body.addressInput, req.params.propertyID]
-        console.log(req.body, req.params);
-        sql = db.pool.query (sql, inserts, function(error, results, fields){
-            if(error){
-                res.write(JSON.stringify(error));
-                res.end();
-            }else{
-                res.redirect('/properties');
-            }
-        })
+app.post('/propertiesUpdate/:propertyID', function(req, res){
+    var mysql = req.app.get('mysql');
+    var sql = "UPDATE Properties SET propAddress = ? WHERE propertyID = ?;";
+    var inserts = [req.body.addressInput, req.params.propertyID]
+    console.log(req.body, req.params);
+    sql = db.pool.query (sql, inserts, function(error, results, fields){
+        if(error){
+            res.write(JSON.stringify(error));
+            res.end();
+        }else{
+            res.redirect('/properties');
+        }
     })
+})
+
+app.get('/properties/delete/:propertyID', function(req, res) {
+	deleteProperties(req.params.id, res);
+	res.redirect('/character');
+});
 
 /*
     LISTENER
