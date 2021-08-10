fb2760acb8de309c2431fb12e599f007e5174333
costjose <costjose@oregonstate.edu>
2021-08-09 20:48:38 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var app     = express();
 var db = require('./database/db-connector');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
-PORT        = 5478;
+PORT        = 5479;
 app.engine('.hbs', exphbs({                     
     extname: ".hbs"
 }));
